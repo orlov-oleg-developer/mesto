5fafad0fb2b7be13bd84c5e6307e1a035fd2ac53
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2022-07-08 21:59:49 +0300

    hotfix: внес исправления в скрипты

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,25 @@
+// VARIABLES
+
 const selectors = {
   profile: '.profile',
   profileTitle: '.profile__title',
   profileJob: '.profile__subtitle',
-  editButton: '.profile__edit-button',
+  buttonEdit: '.profile__edit-button',
 
   profilePopup: '.popup_purpose_profile',
-  closeProfilePopupButton: '.popup__close-button',
+  buttonCloseProfilePopup: '.popup__close-button',
 
   profileForm: '.popup__form',
-  profileNameInput: '.popup__text-input_type_name',
-  profileJobInput: '.popup__text-input_type_description',
+  profileFormNameInput: '.popup__text-input_type_name',
+  profileFormJobInput: '.popup__text-input_type_description',
 
-  addCardButton: '.profile__add-button',
-  addCardPopup: '.popup_purpose_add-cards',
-  closeCardPopupButton: '.popup__close-button',
+  buttonAddCard: '.profile__add-button',
+  popupAddCard: '.popup_purpose_add-cards',
+  buttonCloseAddCardPopup: '.popup__close-button',
 
-  addCardForm: '.popup__form',
-  imageInput:'.popup__text-input_type_image-name',
-  linkInput: '.popup__text-input_type_link',
+  formAddCard: '.popup__form',
+  formImageInput:'.popup__text-input_type_image-name',
+  formLinkInput: '.popup__text-input_type_link',
 
   cardsTemplate: '#cards-template',
   cardsContainer: '.elements__list',
@@ -28,46 +30,10 @@ const selectors = {
   cardTrash: '.elements__trash',
 
   fullScreenPopup: '.popup_purpose_full-screen',
-  closeFullScreenPopupButton: '.full-screen__close-button',
+  buttonCloseFullScreenPopup: '.full-screen__close-button',
   fullScreenImage: '.full-screen__image',
   fullScreenDescription: '.full-screen__description',
 }
-
-// Profile variables
-
-const profile = document.querySelector(selectors.profile);
-let profileTitle = profile.querySelector(selectors.profileTitle);
-let profileJob = profile.querySelector(selectors.profileJob);
-const editButton = profile.querySelector(selectors.editButton);
-
-const profilePopup = document.querySelector(selectors.profilePopup);
-const closeProfilePopupButton = profilePopup.querySelector(selectors.closeProfilePopupButton);
-const profileForm = profilePopup.querySelector(selectors.profileForm);
-let profileNameInput = profileForm.querySelector(selectors.profileNameInput);
-let profileJobInput = profileForm.querySelector(selectors.profileJobInput);
-
-// Cards variables
-
-const addCardButton = profile.querySelector(selectors.addCardButton);
-const addCardPopup = document.querySelector(selectors.addCardPopup);
-const closeCardPopupButton = addCardPopup.querySelector(selectors.closeCardPopupButton);
-
-const addCardForm = addCardPopup.querySelector(selectors.addCardForm);
-let imageInput = addCardPopup.querySelector(selectors.imageInput);
-let linkInput = addCardPopup.querySelector(selectors.linkInput);
-
-const cardsContainer = document.querySelector(selectors.cardsContainer);
-
-// Full screen variables
-
-const fullScreenPopup = document.querySelector(selectors.fullScreenPopup);
-const closeFullScreenPopupButton = fullScreenPopup.querySelector(selectors.closeFullScreenPopupButton);
-const fullScreenImage = fullScreenPopup.querySelector(selectors.fullScreenImage);
-const fullScreenDescription = fullScreenPopup.querySelector(selectors.fullScreenDescription);
-
-// MAIN LOGIC
-
-// Cards creation
 
 const initialCards = [
   {
@@ -96,16 +62,134 @@ const initialCards = [
   }
 ];
 
+// Profile variables
+
+const profile = document.querySelector(selectors.profile);
+const profileTitle = profile.querySelector(selectors.profileTitle);
+const profileJob = profile.querySelector(selectors.profileJob);
+const buttonEdit = profile.querySelector(selectors.buttonEdit);
+
+const profilePopup = document.querySelector(selectors.profilePopup);
+const buttonCloseProfilePopup = profilePopup.querySelector(selectors.buttonCloseProfilePopup);
+const profileForm = profilePopup.querySelector(selectors.profileForm);
+const profileFormNameInput = profileForm.querySelector(selectors.profileFormNameInput);
+const profileFormJobInput = profileForm.querySelector(selectors.profileFormJobInput);
+
+// Cards variables
+
+const buttonAddCard = profile.querySelector(selectors.buttonAddCard);
+const popupAddCard = document.querySelector(selectors.popupAddCard);
+const buttonCloseAddCardPopup = popupAddCard.querySelector(selectors.buttonCloseAddCardPopup);
+
+const formAddCard = popupAddCard.querySelector(selectors.formAddCard);
+const formImageInput = popupAddCard.querySelector(selectors.formImageInput);
+const formLinkInput = popupAddCard.querySelector(selectors.formLinkInput);
+
+const cardsContainer = document.querySelector(selectors.cardsContainer);
+
+// Full screen variables
+
+const fullScreenPopup = document.querySelector(selectors.fullScreenPopup);
+const buttonCloseFullScreenPopup = fullScreenPopup.querySelector(selectors.buttonCloseFullScreenPopup);
+const fullScreenImage = fullScreenPopup.querySelector(selectors.fullScreenImage);
+const fullScreenDescription = fullScreenPopup.querySelector(selectors.fullScreenDescription);
+
+// FUNCTIONS
+
+function createCard(data) {
+  const cardTemplate = document.querySelector(selectors.cardsTemplate).content;
+  const cardElement = cardTemplate.querySelector(selectors.cardsElement).cloneNode(true);
+  const cardImage = cardElement.querySelector(selectors.cardImage);
+  const nameForCard = data.name;
+
+  cardImage.src = data.link;
+  cardImage.alt = nameForCard;
+  cardElement.querySelector(selectors.cardTitle).textContent = nameForCard;
+  cardElement.querySelector(selectors.cardHeart).addEventListener('click', toggleLike);
+
+  cardElement.querySelector(selectors.cardTrash).addEventListener('click', removeCard);
+
+  cardImage.addEventListener('click', openFullScreen);
+
+  return cardElement;
+}
+
+function addCard(data, cardsContainer) {
+  const card = createCard(data);
+  cardsContainer.prepend(card);
+}
+
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function openProfilePopup() {
+  openPopup(profilePopup)
+  profileFormNameInput.value = profileTitle.textContent;
+  profileFormJobInput.value = profileJob.textContent;
+}
+
+function closeProfilePopup() {
+  closePopup(profilePopup);
+}
+
+function changeProfileInformation(event) {
+  event.preventDefault();
+
+  profileTitle.textContent = profileFormNameInput.value;
+  profileJob.textContent = profileFormJobInput.value;
+
+  closeProfilePopup();
+}
+
+function openAddCardPopup() {
+  openPopup(popupAddCard);
+}
+
+function closeAddCardPopup() {
+  closePopup(popupAddCard);
+}
+
+function openFullScreen(event) {
+  fullScreenImage.src = event.target.src;
+  fullScreenImage.alt = event.target.alt;
+  fullScreenDescription.textContent = event.target.alt;
+  openPopup(fullScreenPopup);
+}
+
+function closeFullScreen() {
+  closePopup(fullScreenPopup);
+}
+
+function toggleLike (event) {
+  event.target.classList.toggle('elements__element-heart_active');
+}
+
+function removeCard(event) {
+  event.target.closest(selectors.cardsElement).remove();
+}
+
+// MAIN LOGIC
+
+// Cards creation
+
 initialCards.forEach(function (item) {
   addCard({name: item.name,link: item.link}, cardsContainer);
 });
 
 // Profile
 
-editButton.addEventListener('click', openProfilePopup);
+buttonEdit.addEventListener('click', openProfilePopup);
 
-closeProfilePopupButton.addEventListener('click', closeProfilePopup);
+buttonCloseProfilePopup.addEventListener('click', closeProfilePopup);
 
+/* Цель обработчика - закрыть попап при клике вне формы.
+Используется событие mousedown, а не click, так как такой подход защищает от следующего сценария:
+пользователь начал выделять мышкой текстовое поле и отпустил мышь вне формы - форма закрылась. */
 profilePopup.addEventListener('mousedown', function(event) {
   if (event.target !== event.currentTarget) {
     return;
@@ -117,96 +201,23 @@ profileForm.addEventListener('submit', changeProfileInformation);
 
 // Add new cards
 
-addCardButton.addEventListener('click', openAddCardPopup);
+buttonAddCard.addEventListener('click', openAddCardPopup);
 
-closeCardPopupButton.addEventListener('click', closeAddCardPopup);
+buttonCloseAddCardPopup.addEventListener('click', closeAddCardPopup);
 
-addCardPopup.addEventListener('mousedown', function(event) {
+popupAddCard.addEventListener('mousedown', function(event) {
   if (event.target !== event.currentTarget) {
     return;
   }
   closeAddCardPopup();
 })
 
-addCardForm.addEventListener('submit', function (event) {
+formAddCard.addEventListener('submit', function (event) {
   event.preventDefault();
-  addCard({name: imageInput.value, link: linkInput.value}, cardsContainer);
+  addCard({name: formImageInput.value, link: formLinkInput.value}, cardsContainer);
   closeAddCardPopup();
 });
 
 // Full screen logic
 
-closeFullScreenPopupButton.addEventListener('click', closeFullScreen);
-
-// FUNCTIONS
-
-function createCard(data) {
-  const cardTemplate = document.querySelector(selectors.cardsTemplate).content;
-  const cardElement = cardTemplate.querySelector(selectors.cardsElement).cloneNode(true);
-
-  cardElement.querySelector(selectors.cardImage).src = data.link;
-  cardElement.querySelector(selectors.cardImage).alt = data.name;
-  cardElement.querySelector(selectors.cardTitle).textContent = data.name;
-  cardElement.querySelector(selectors.cardHeart).addEventListener('click', toggleLike);
-
-  cardElement.querySelector(selectors.cardTrash).addEventListener('click', removeCard);
-
-  cardElement.querySelector(selectors.cardImage).addEventListener('click', openFullScreen);
-
-  return cardElement;
-}
-
-function addCard(data, cardsContainer) {
-  const card = createCard(data);
-  cardsContainer.prepend(card);
-}
-
-function openProfilePopup() {
-  profilePopup.classList.add('popup_opened');
-
-  profileNameInput.value = profileTitle.textContent;
-  profileJobInput.value = profileJob.textContent;
-}
-
-function closeProfilePopup() {
-  profilePopup.classList.remove('popup_opened');
-}
-
-function changeProfileInformation(event) {
-  event.preventDefault();
-
-  profileTitle.textContent = profileNameInput.value;
-  profileJob.textContent = profileJobInput.value;
-
-  closeProfilePopup();
-}
-
-function openAddCardPopup() {
-  addCardPopup.classList.add('popup_opened');
-}
-
-function closeAddCardPopup() {
-  addCardPopup.classList.remove('popup_opened');
-}
-
-function openFullScreen(event) {
-  fullScreenImage.src = event.target.src;
-  fullScreenImage.alt = event.target.nextElementSibling.textContent;
-  fullScreenDescription.textContent = event.target.nextElementSibling.textContent;
-  fullScreenPopup.classList.add('full-screen_opened');
-}
-
-function closeFullScreen() {
-  fullScreenPopup.classList.remove('full-screen_opened');
-}
-
-function toggleLike (event) {
-  event.target.classList.toggle('elements__element-heart_active');
-}
-
-function removeCard(event) {
-  event.target.closest(selectors.cardsElement).remove();
-}
-
-
-
+buttonCloseFullScreenPopup.addEventListener('click', closeFullScreen);
