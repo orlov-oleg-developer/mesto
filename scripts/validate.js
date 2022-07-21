5dab7ae65ae70b66f1248f028dd1824e3ead913f
Orlov Oleg <61846414+orlov-oleg-developer@users.noreply.github.com>
2022-07-21 09:49:13 +0300

    feature: внес изменения в функционал валидации

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,5 +1,4 @@
 // VARIABLES
-
 const validateSelectors = {
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -9,20 +8,7 @@ const validateSelectors = {
   errorClass: 'popup__input-error_active'
 };
 
-const showInputError = (formElement, inputElement, errorMessage, selectors) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(selectors.inputErrorClass);
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add(selectors.errorClass);
-};
-
-const hideInputError = (formElement, inputElement, selectors) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove(selectors.inputErrorClass);
-  errorElement.classList.remove(selectors.errorClass);
-  errorElement.textContent = '';
-};
-
+// Проверяем поле формы на валидность
 const checkInputValidity = (formElement, inputElement, selectors) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage, selectors);
@@ -31,6 +17,41 @@ const checkInputValidity = (formElement, inputElement, selectors) => {
   }
 };
 
+// Меняем цвет поля формы на красны и добавляем текст ошибки под ним
+const showInputError = (formElement, inputElement, errorMessage, selectors) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(selectors.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(selectors.errorClass);
+};
+
+// Меняем цвет поля формы на черный и убераем текст ошибки под ним
+const hideInputError = (formElement, inputElement, selectors) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(selectors.inputErrorClass);
+  errorElement.classList.remove(selectors.errorClass);
+  errorElement.textContent = '';
+};
+
+// Проверяем форму на валидность. Если все поля формы валидны, то возвращается true
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  })
+}
+
+// Изменяем статус кнопки отправки формы
+const toggleButtonState = (inputList, buttonElement, selectors) => {
+  if(hasInvalidInput(inputList)) {
+    buttonElement.setAttribute('disabled', true);
+    buttonElement.classList.add(selectors.inactiveButtonClass);
+  } else {
+    buttonElement.removeAttribute('disabled');
+    buttonElement.classList.remove(selectors.inactiveButtonClass);
+  }
+}
+
+// Подключаем валидацию к каждому полю ввода формы, полученной на входе
 const setEventListeners = (formElement, selectors) => {
   const inputList = Array.from(formElement.querySelectorAll(selectors.inputSelector));
   const buttonElement = formElement.querySelector(selectors.submitButtonSelector);
@@ -45,6 +66,7 @@ const setEventListeners = (formElement, selectors) => {
   });
 };
 
+// Подключаем валидацию к каждой форме документа
 const enableValidation = (selectors) => {
   const formList = Array.from(document.querySelectorAll(selectors.formSelector));
 
@@ -57,20 +79,6 @@ const enableValidation = (selectors) => {
   });
 };
 
-const hasInvalidInput = (inputList) => {
-  return inputList.some((input) => {
-    return !input.validity.valid;
-  })
-}
-
-const toggleButtonState = (inputList, buttonElement, selectors) => {
-  if(hasInvalidInput(inputList)) {
-    buttonElement.setAttribute('disabled', true);
-    buttonElement.classList.add(selectors.inactiveButtonClass);
-  } else {
-    buttonElement.removeAttribute('disabled');
-    buttonElement.classList.remove(selectors.inactiveButtonClass);
-  }
-}
+// MAIN LOGIC
 
 enableValidation(validateSelectors);
